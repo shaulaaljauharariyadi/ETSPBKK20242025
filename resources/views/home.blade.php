1bbe8e385480d407584bdb0071bab55fdc188fe3
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-11 15:43:08 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,10 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot> 
   <h3 class="text-xl">Halaman Home Page</h3>
+  <br>
+  <img src="{{ asset('images/Cinderella_FB.jpg') }}" alt="Gambar Deskripsi" style="margin-top: 5px;
+  margin-right: 50px;
+  margin-bottom: 5px;
+  margin-left: 50px;
+  " width="475">
 </x-layout>

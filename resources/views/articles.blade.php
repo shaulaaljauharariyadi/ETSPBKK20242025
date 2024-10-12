6cdad801b7c564cde6a22ab5ade050badee0bfe3
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-13 01:27:44 +0700

    Update articles.blade.php

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -8,7 +8,7 @@
     <h2 class="mb-1 text-3xl track-tight font-bold text-gray-900">{{ $article ['title'] }}</h2>
     </a>
     <div class="text-base text-gray-500">
-      <a href="#">{{ $article ['author'] }}</a> | 1 januari 2024
+      <a href="#">{{ $article ['author'] }}</a> | {{ $article->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light">{{ Str::limit($article ['body'], 100) }}</p>
       <a href="/articles/{{ $article['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>

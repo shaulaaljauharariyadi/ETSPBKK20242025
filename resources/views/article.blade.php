836053434b658b9b749c695ae350c154de99c4f1
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-12 19:02:03 +0700

    Update and rename article.blade.php to articles.blade.php

--- resources/views/article.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-  <x-slot:title>{{ $title }}</x-slot> 
-  <h3 class="text-xl">Halaman Artikel</h3>
-</x-layout>
---
+++ resources/views/articles.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot>
+
+
+  @foreach ($articles as $article)
+  <article class="py-8 max-w-screen-md border-b border-gray-500">
+    <a href="/articles/{{ $article['slug'] }}" class="hover:underline">
+    <h2 class="mb-1 text-3xl track-tight font-bold text-gray-900">{{ $article ['title'] }}</h2>
+    </a>
+    <div class="text-base text-gray-500">
+      <a href="#">{{ $article ['author'] }}</a> | 1 januari 2024
+    </div>
+    <p class="my-4 font-light">{{ Str::limit($article ['body'], 100) }}</p>
+      <a href="/articles/{{ $article['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+  </article>
+  @endforeach
+
+</x-layout>

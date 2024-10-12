bd32611dd2a64234c4b7f3537b72ae48af706739
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-12 18:55:58 +0700

    Update layout.blade.php

--- resources/components/layout.blade.php
+++ resources/components/layout.blade.php
@@ -7,11 +7,7 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Home</title>
-    <style>
-        body {
-            background-color: pink;
-        }
-    </style>
+    
 </head>
 
 <body class="h-full">

f559bea1793f424d1a249d2e894c75c35f57a420
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-14 01:29:18 +0700

    Update 2024_10_12_165903_create_articles_table.php

--- database/migrations/2024_10_12_165903_create_articles_table.php
+++ database/migrations/2024_10_12_165903_create_articles_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
+            $table->foreignId('author_id')->constrained(
+                table:'users',
+                indexName:'articles_author_id'
+            );
             $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();

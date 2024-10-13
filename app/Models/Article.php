362d486add7c62f0191f3b2667b592d22aeb3ae1
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-14 01:34:15 +0700

    Update Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    public function author(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilter(Builder $query): void {
+
+        $query->when( $filters['search'] ?? false, fn($query, $search)=>
+            $query->where('title', 'like', '%' . $search . '%')
+        );
+        
+    }
 }

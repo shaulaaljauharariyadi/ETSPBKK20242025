d45b0ebf67701d79fa6f8b3d93659771056b55b9
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-14 01:38:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Article;
+use App\Models\User;
 
 
 Route::get('/', function () {
@@ -14,10 +15,19 @@ Route::get('/about', function () {
 });
 
 Route::get('/articles', function () {
-    return view('articles',['title' => 'Artikel', 'articles' => Article::all()]);
+    
+
+    return view('articles',['title' => 'Artikel', 'articles' => Article::filter()->latest()->paginate(9)]);
+
+    
 });
 
 Route::get('/articles/{article:slug}', function(Article $article) {
 
         return view('article', ['title' => 'Single Article', 'article' => $article]);
+});
+
+Route::get('/authors/{user}', function(User $user) {
+
+    return view('articles', ['title' => 'Articles by '. $user->name, 'articles' => $user->articles]);
 });

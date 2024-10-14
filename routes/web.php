b511a55ebca76d4800bb8c702b82ab8d97f21f18
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-14 23:53:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Article;
@@ -31,3 +32,8 @@ Route::get('/authors/{user}', function(User $user) {
 
     return view('articles', ['title' => 'Articles by '. $user->name, 'articles' => $user->articles]);
 });
+
+Route::get('/user', [UserController::class, 'tampil'])->name('user.tampil');
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('user.tambah');
+Route::post('/user/submit', [UserController::class, 'submit'])->name('user.submit');
+Route::get('/user/edit', [UserController::class, 'edit'])->name('user.edit');
